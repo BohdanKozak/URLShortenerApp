d327c2494c1ca8185fa473055ed6eedfb2e53ca6
Kozak Bohdan <bohdankozak90@gmail.com>
2023-08-11 13:50:55 +0300

    Add Algoritm Description to App

---
+++ URLShortener/Controllers/DescriptionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Models;
+
+namespace URLShortener.Controllers
+{
+    public class DescriptionController : Controller
+    {
+        static string fileName = "Description.txt";
+        static string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+
+
+        private DescriptionModel? _description;
+
+        public DescriptionController()
+        {
+
+
+            //_description = DescriptionModel.LoadFromFile(Path.Combine(filePath, fileName));
+
+        }
+
+        public IActionResult Index()
+        {
+            var editableContent = DescriptionModel.LoadFromFile(filePath);
+
+            return View(editableContent);
+        }
+
+
+
+    }
+}
---
+++ UrlShortener.Models/DescriptionModel.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace UrlShortener.Models
+{
+    public class DescriptionModel
+    {
+        public DescriptionModel(string text, string title, string author)
+        {
+            Text = text;
+            Title = title;
+            Author = author;
+        }
+
+        public string Text { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public void SaveToFile(string filePath)
+        {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static DescriptionModel? LoadFromFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<DescriptionModel>(json);
+            }
+            return null;
+        }
+    }
+
+}
